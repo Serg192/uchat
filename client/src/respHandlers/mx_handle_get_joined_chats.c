e073a1ada9d71ed6d543d3e615adaee93ef1fc25
Trubadur <ozelenskyj@stud.khpi.ucode-connect.study>
2023-03-10 13:05:43 +0300

    added color icon

--- client/src/respHandlers/mx_handle_get_joined_chats.c
+++ client/src/respHandlers/mx_handle_get_joined_chats.c
@@ -1,9 +1,32 @@
 #include "../../inc/client.h"
 
+gboolean draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data) {
+	guint width, height;
+	GdkRGBA color;
+	GtkStyleContext *context;
+	
+	gdk_rgba_parse(&color, "rgba(255,0,0,1)");
+
+	width = gtk_widget_get_allocated_width(widget);
+	height = gtk_widget_get_allocated_height(widget);
+	context = gtk_widget_get_style_context(widget);
+
+	gtk_render_background(context, cr, 0, 0, width, height);
+
+	cairo_arc (cr, width / 2.0, height / 2.0,
+             	MIN(width, height) / 2.0, 0, 2 * G_PI);
+
+	gdk_cairo_set_source_rgba(cr, &color);
+
+	cairo_fill(cr);
+
+	return FALSE;
+}
+
 static inline void update_chat_list(client_t* client) {
 
 	GtkWidget *button;
-	GtkWidget *image;
+	GtkWidget *icon;
 
 	gtk_widget_hide(client->c_window->chats_list_grid);
 
@@ -14,16 +37,26 @@ static inline void update_chat_list(client_t* client) {
 		gtk_grid_insert_row(GTK_GRID(client->c_window->chats_list_grid), i);
 
 		button = gtk_button_new_with_label(client->chat_container->chats[i]->name);
-		image = gtk_image_new_from_file("client/src/input-mouse.png");
+		char *str = mx_strndup(client->chat_container->chats[i]->name, 2);
+		str[0] = (char)mx_toupper((int)str[0]);
+		str[1] = (char)mx_toupper((int)str[1]);
+		icon = gtk_label_new(str);
 
-		gtk_button_set_image(GTK_BUTTON(button), image);
+		gtk_widget_set_size_request(icon, 50, 50);
+		GdkRGBA icon_text_color;
+		gdk_rgba_parse(&icon_text_color, "rgba(255,255,255,1)");
+		gtk_widget_override_color(GTK_WIDGET(icon), GTK_STATE_FLAG_NORMAL, &icon_text_color);
+		
+		gtk_button_set_image(GTK_BUTTON(button), icon);
 		gtk_button_set_always_show_image(GTK_BUTTON(button), TRUE);
 		gtk_widget_set_size_request(button, 200, 100);
 		gtk_button_set_alignment(GTK_BUTTON(button), 0.0, 0.5);
 		gtk_grid_attach(GTK_GRID(client->c_window->chats_list_grid), button, 1, i, 1, 1);
 
 
+		g_signal_connect(G_OBJECT(icon), "draw", G_CALLBACK(draw_callback), NULL);
 		g_signal_connect(button, "released", G_CALLBACK(mx_on_chat_btn_clicked), client->chat_container->chats[i]);
+		mx_strdel(&str);
 	}
 
 	client->c_window->elements_were_attached = client->chat_container->count;
